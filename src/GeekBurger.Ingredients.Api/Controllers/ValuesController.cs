441b7deec1939a883d1889043290e3d1b9f8a65c
Jéssica Nathany <jessicanathany.web@gmail.com>
2018-03-01 21:05:10 -0300

    criação da estrutura do projeto

--- src/GeekBurger.Ingredients.Api/Controllers/ValuesController.cs
+++ src/GeekBurger.Ingredients.Api/Controllers/ValuesController.cs
@@ -16,6 +16,7 @@ namespace GeekBurger.Ingredients.Api.Controllers
             return new string[] { "value1", "value2" };
         }
 
+
         // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
