78c704b19ae860d2c05b9c9eed15410f7956f6b4
Reginaldo Alves de Barros <R21Regis@hotmail.com>
2018-03-06 23:15:52 -0300

    Added ingredients controller and mock repository

---
+++ src/GeekBurger.Ingredients.Api/Controllers/IngredientsController.cs
@@ -0,0 +1,25 @@
+﻿using GeekBurger.Ingredients.Api.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace GeekBurger.Ingredients.Api.Controllers
+{
+    [Route("ingredients")]
+    public class IngredientsController : Controller
+    {
+        private readonly MockRepository _mockRepository;
+
+        public IngredientsController(MockRepository mockRepository)
+        {
+            _mockRepository = mockRepository;
+        }
+
+        [HttpGet("{productId}")]
+        public IActionResult Get(Guid productId)
+        {
+            var ingredients =_mockRepository.GetIngredients(productId);
+
+            return Ok(ingredients);
+        }
+    }
+}
---
+++ src/GeekBurger.Ingredients.Api/Data/MockRepository.cs
@@ -0,0 +1,20 @@
+﻿using GeekBurger.Ingredients.Contracts;
+using System;
+using System.Collections.Generic;
+
+namespace GeekBurger.Ingredients.Api.Data
+{
+    public class MockRepository
+    {
+        public IEnumerable<Ingredient> GetIngredients(Guid productId)
+        {
+            return new List<Ingredient>()
+            {
+                Ingredient.Create(productId, "Açucar"),
+                Ingredient.Create(productId, "Leite"),
+                Ingredient.Create(productId, "Farinha de Trigo"),
+                Ingredient.Create(productId, "Sal"),
+            };
+        }
+    }
+}
