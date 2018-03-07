1daaa1cfb622e49c2354537a539a034f6b9d32e6
Reginaldo Alves de Barros <R21Regis@hotmail.com>
2018-03-06 22:43:10 -0300

    Added  contract model

---
+++ src/GeekBurger.Ingredients.Contracts/Ingredient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GeekBurger.Ingredients.Contracts
+{
+    public class Ingredient
+    {
+        public Guid IngredientId { get; }
+
+        public Guid ProductId { get; set; }
+
+        public string Description { get; set; }
+    }
+}
